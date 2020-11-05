7f593de3183adc764da6f6e8a28a1252a3bddb16
karsanovt <48289990+karsanovt@users.noreply.github.com>
2020-11-05 23:58:48 +0300

    Add files via upload

---
+++ Token_stream.h
@@ -0,0 +1,31 @@
+﻿#ifndef TOKEN_STREAM_H
+#define TOKEN_STREAM_H
+
+#include "Token.h"
+
+class Token_stream
+{
+private:
+  bool full{ false };
+  Token buffer{ '\0' };
+public:
+  Token_stream () { }
+  Token get ();
+  void putback (Token t);
+  void ignore (char c);
+};
+
+constexpr char quit = 'q';
+constexpr char print = ';';
+constexpr char number = '8';
+constexpr char name = 'a';
+constexpr char let = 'L';
+
+const string prompt = "> ";
+const string result = "= ";
+const string declkey = "let";
+const string help = "help";
+const string Exit = "exit";
+const string showval = "show";
+
+#endif // TOKEN_STREAM_H
---
+++ funcs.h
@@ -0,0 +1,16 @@
+﻿#ifndef FUNCS_H
+#define FUNCS_H
+
+#include"Variable.h"
+#include"Token_stream.h"
+
+double expression();
+double primary();
+double term();
+double declaration(Token let_or_const);
+double statement();
+void clean_up_mess();
+void printHelp();
+void calculate();
+
+#endif // FUNCS_H
