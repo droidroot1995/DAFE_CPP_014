a9b6b64f62bc39ba6a0d6a9873df4bf264f111ea
karsanovt <48289990+karsanovt@users.noreply.github.com>
2020-12-10 00:21:12 +0300

    vector_v_1 Карсанов Т.

--- main.cpp
+++ main.cpp
@@ -3,96 +3,44 @@
 using namespace std;
 
 class vectop
-{
-protected:
+        {
     int sz;
     double* elem;
 public:
     vectop(int s)
     {
         sz = s;
-        elem = new double[sz];
-        for(int i = 0; i < sz; ++i)
+        elem = new double[s];
+        for(int i = 0; i < s; ++i)
             elem[i] = 0.0;
     }
     int size() const
     {
         return sz;
     }
-    vectop(initializer_list<double> l)
-    {
-        sz = l.size();
-        elem = new double[sz];
-        copy (l.begin (), l.end () , elem) ;
-    }
-    vectop (vectop&& a);
-    vectop (const vectop&);
-
-    vectop& operator = (const vectop&);
-    vectop& operator = (vectop&&);
-    double& operator [] (int n)
+    double getter(int n)
     {
         return elem[n];
     }
-    double operator [] (int n) const;
-
+    void setter(int n, double v)
+    {
+        elem[n] = v;
+    }
     ~vectop()
     {
-        delete[] elem;
+        delete [] elem;
     }
 };
 
-vectop::vectop (vectop&& a)
-{
-    sz = a.sz;
-    elem = a.elem;
-    a.sz = 0;
-    a.elem = nullptr;
-}
-
-vectop::vectop(const vectop& arg)
-{
-    sz = arg.sz;
-    elem = new double[sz];
-    copy(arg.elem, arg.elem+arg.sz, elem);
-}
-
-vectop& vectop::operator = (const vectop& a)
-{
-    double *p = new double [a.sz];
-    copy (a.elem, a.elem+a.sz ,p);
-    delete [] elem;
-    elem = p;
-    sz = a.sz;
-    return *this;
-}
-
-vectop& vectop::operator = (vectop&& a)
-{
-    delete[] elem;
-    elem = a.elem;
-    sz = a.sz;
-    a.elem = nullptr;
-    a.sz = 0;
-    return *this;
-}
-
 int main()
 {
-    vectop v1 (20);
-    cout << "v1"<<'\n';
+    vectop v1(6);
+    v1.setter(0,0.0);
+    v1.setter(1,0.1);
+    v1.setter(2,0.2);
+    v1.setter(3,0.3);
+    v1.setter(4,0.4);
+    v1.setter(5,0.5);
     for (int i = 0; i < v1.size(); i++)
-    {
-        v1[i] = i * i;
-        cout << v1[i] << ' ';
-    }
-    cout<<'\n';
-
-    cout << "======================================" << '\n';
-
-    const vectop v2 = {1.2,2.5,3.5,4.8,5};
-    v1 = v2;
-    for (int i = 0; i < v1.size(); i++)
-        cout<<v1[i]<<' ';
-    return 0;
+        cout << v1.getter(i) << '\n';
 }
